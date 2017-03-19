c02d8664a9e08a05c6fe35303cd968130068f6bf
MM <mitasoft1@gmail.com>
2017-03-19 13:48:32 +0200

    playlist page

--- playlistPageViewModel.js
+++ playlistPageViewModel.js
@@ -1,9 +1,22 @@
 function PlaylistPageViewModel() {
     var self = this;
     self.playlists = ko.observableArray([]);
+    self.cautaToken = ko.observable();
 
-    self.init = function() {
-         self.playlists(Repo.ContentItems.getAllPlaylists());
+    self.init = function () {
+        self.playlists(Repo.ContentItems.getAllPlaylists());
+    }
+
+    self.onCauta = function () {
+        toastr.info('Cauta ....');
+        console.log(self.cautaToken());
+    }
+
+    self.onEnter = function (model, event) {
+        if (event.keyCode == 13) {
+            debugger;
+        }
+        return true;
     }
 
     self.init();
